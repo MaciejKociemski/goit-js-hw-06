545e73f374db8c531ddd6c183e3f20cb892f0112
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-02-11 14:43:44 +0100

    change const

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const categoriesRef = document.querySelector("#categories")
 console.log("Number of categories:", categoriesRef.childElementCount)
 
 for (const element of categoriesRef.children) {
-    const categoryHeading = element.querySelector('h2').textContent
+    const categoryHeading = element.querySelector('li').textContent
     const itemQuantity = element.querySelector('ul').childElementCount
 
     console.log("Category:", categoryHeading)
