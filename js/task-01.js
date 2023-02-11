765b2e940d7441f5488c1f38ff79467ece824999
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-02-11 17:08:05 +0100

    added tasks done
    form 1 to 4 (4 for check)

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ console.log("Number of categories:", categoriesRef.childElementCount)
 
 for (const element of categoriesRef.children) {
     const categoryHeading = element.querySelector('h2').textContent
-    const itemQuantity = element.querySelector('ul').childElementCount
+    const itemQuantity = element.querySelector("ul").childElementCount
 
     console.log("Category:", categoryHeading)
     console.log("Elements:", itemQuantity)
