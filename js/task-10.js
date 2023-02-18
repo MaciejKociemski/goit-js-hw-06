912ed90f93dbcf346af90bf93a830d181ad1fe26
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-02-18 14:27:02 +0100

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,14 +2,35 @@
 //getRandomHexColor, Math.random, [data-create, data-destroy], div 30x30
 
 
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
- 
-
 const createBtn = document.querySelector('[data-create]')
+const destroyBtn = document.querySelector('[data-destroy]')
 
 
+const inputRef = document.querySelector('input')
+const boxesRef = document.querySelector('#boxes')
+
+const createBoxes = (amount) => {
+  const boxes = []
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div")
+    box.style.backgroundColor = getRandomHexColor()
+    box.style.width = `${30 + i * 10}px`
+    box.style.height = `${30+i*10}px`
+    boxes.push(box)
+  }
+
+  boxesRef.append(...boxes)
+}
+const onCreate = () => {
+  createBoxes(inputRef.value)
+}
+const destroyBoxes = () => {
+  boxesRef.innerHTML = ""
+}
+
+createBtn.addEventListener("click", onCreate)
+destroyBtn.addEventListener("click", destroyBoxes)
