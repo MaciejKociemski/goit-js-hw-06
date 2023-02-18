4b3c0f88e751d102b80fc079b898b33e061dc552
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-02-18 09:49:07 +0100

    summary

--- js/task-10.js
+++ js/task-10.js
@@ -1,1 +1,15 @@
-//Zadanie 10 (nieobowiązkowe)​ probably too difficult at this stage for me
+//Zadanie 10 (nieobowiązkowe)​ 
+//getRandomHexColor, Math.random, [data-create, data-destroy], div 30x30
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+ 
+
+const createBtn = document.querySelector('[data-create]')
+
+
